8bc2fb340b1a32be04bfec074759e59d10c7c4d9
maryam <maryam.ghayoomi@gmail.com>
2025-04-30 12:06:59 +0200

    implements Lab: Build a Spring Boot Book List Web App

---
+++ labs/booklist/src/main/java/com/example/booklist/model/Book.java
@@ -0,0 +1,81 @@
+package com.example.booklist.model;
+
+import org.springframework.stereotype.Component;
+
+public class Book {
+    private String title;
+    private String author;
+    private int year;
+    private String genre;
+    private double price;
+    private boolean inStock;
+
+    public Book(String title, String author, int year, String genre, double price, boolean inStock) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+        this.genre = genre;
+        this.price = price;
+        this.inStock = inStock;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public boolean isInStock() {
+        return inStock;
+    }
+
+    public void setInStock(boolean inStock) {
+        this.inStock = inStock;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", year=" + year +
+                ", genre='" + genre + '\'' +
+                ", price=" + price +
+                ", inStock=" + inStock +
+                '}';
+    }
+}
